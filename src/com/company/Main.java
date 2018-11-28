811031bb57521224567f21587e459b72dd376e40
fvictoria <felipevictoria630@yahoo.com>
2018-11-27 21:42:57 -0500

    almost?

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MonteCarlo mcObj = new MonteCarlo(3,5,2);
+        int sqrCount = 0,cirCount = 0;
+
+        for(int i = 0; i <100;i++)
+        {
+            //.....
+
+            cirCount++;
+            sqrCount++;
+        }
+
+
+
     }
 }
---
+++ src/com/company/MonteCarlo.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.Random;
+public class MonteCarlo {
+
+    public double h,k,r;
+
+    private Random rndm = new Random();
+    public MonteCarlo(double height, double width, double radius)
+    {
+        h = height;
+        k  = width;
+        r = radius;
+    }
+    public double nextRainDrop_x()
+    {
+        return rndm.nextDouble()*k;
+    }
+    public double nextRainDrop_y()
+    {
+        return rndm.nextDouble()*h;
+    }
+    public boolean insideCircle(double x,double y)
+    {
+        if (Math.pow(x-h,2)+Math.pow(y-k,2)<=Math.pow(r,2))
+        return true;
+        else if(!(Math.pow(x-h,2)+Math.pow(y-k,2)<=Math.pow(r,2)));
+        return false;
+    }
+
+
+}
